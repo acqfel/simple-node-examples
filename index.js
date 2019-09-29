d01116673b54441a2fdee582396d5c059a0f9d9c
Felipe Acquati <felipe_acq@yahoo.com.br>
2019-09-29 18:45:09 -0300

    Added: callbacks, error handling

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ let rect = require('./rectangle');
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = "+
                 l + " | b = " + b);
-    if ( l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero");
-        console.log("l = "+l);
-        console.log("b = "+b);
-    } else {
-        console.log("Rectangle area = "+rect.area(l,b));
-        console.log("Rectangle perimete = "+rect.perimeter(l,b));
-    } 
+     rect(l,b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("Rectangle dimensions is l="+l+" and b="+b);
+            console.log("Rectangle area = "+rectangle.area());
+            console.log("Rectangle perimeter = "+rectangle.perimeter());
+        }
+     });
+    console.log("This statement is after the call to rect()");
     
 }
 
 solveRect(2,4);
 solveRect(-5,5);
 solveRect(0,6);
+/*
+if ( l <= 0 || b <= 0) {
+    console.log("Rectangle dimensions should be greater than zero");
+    console.log("l = "+l);
+    console.log("b = "+b);
+} else {
+    console.log("Rectangle area = "+rect.area(l,b));
+    console.log("Rectangle perimete = "+rect.perimeter(l,b));
+}
+*/
--- rectangle.js
+++ rectangle.js
@@ -1,7 +1,8 @@
 module.exports = (x,y,callback) => {
   if ( x <= 0 || y <= 0 ) {
       setTimeout( () => 
-                callback(new Error("Retangle...") ,
+                callback(new Error("Rectangle dimensions should be greater than zero"
+                +" - x = "+x+" and y = "+y),
                 null),
                 2000);
       }
@@ -9,10 +10,10 @@ module.exports = (x,y,callback) => {
         setTimeout(() => 
           callback(null,
           {
-            perimeter: (x, y) => (2*(x+y)),
-            area = (x, y) => (x*y);
+            perimeter: () => (2*(x+y)),
+            area: () => (x*y)
           }),
-          ,2000);
+          2000);
       }
 }
 
